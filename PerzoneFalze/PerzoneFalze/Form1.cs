595311026c47ed66793121f6c398c45d108ba7fc
David Gray <damnedkaiser@gmail.com>
2016-11-04 21:29:41 +0100

    Aggiunto Encoding Unicode per la lettura da file.

--- PerzoneFalze/PerzoneFalze/Form1.cs
+++ PerzoneFalze/PerzoneFalze/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
 
 namespace PerzoneFalze
 {
@@ -19,12 +20,22 @@ namespace PerzoneFalze
         public Form1()
         {
             InitializeComponent();
-            PopulateNamesList();
-            PopulateSurnamesList();
-            Perzona P1 = new Perzona("Luca", "Giordano", new DateTime(1991, 3, 26));
-            //MessageBox.Show(P1.Name + " " + P1.Surname + " " + P1.BirthDate.ToString() + " " + P1.DateAdded.ToString() + " " + P1.LastUpdate.ToString() + " " + P1.DeletedDate.ToString() + " " + P1.StateOfMind.ToString());
-
             List<Perzona> listaPersone = new List<Perzona>();
+            if (PopulateNamesList() && PopulateSurnamesList())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Random rnd = new Random();
+                    int r1 = rnd.Next(NamesList.Count);
+                    int r2 = rnd.Next(SurnamesList.Count);
+                    Perzona P = new Perzona(NamesList[r1], SurnamesList[r2], GenerateRandomBirthDate());
+                    listaPersone.Add(P);
+                    Thread.Sleep(100);
+                }
+            }
+            Perzona P1 = new Perzona("Nicolò", "Giordano", new DateTime(1991, 3, 26));
+
+            
             listaPersone.Add(P1);
 
             using (frmGrigliaPerzone finestraGriglia = new frmGrigliaPerzone(listaPersone))
@@ -37,7 +48,7 @@ namespace PerzoneFalze
             NamesList = new List<string>();
             try
             {
-                StreamReader SR1 = new StreamReader(Directory.GetCurrentDirectory() + "\\TextFiles\\Nomi.txt");
+                StreamReader SR1 = new StreamReader(Directory.GetCurrentDirectory() + "\\TextFiles\\Nomi.txt", Encoding.Unicode);
                 while (!SR1.EndOfStream)
                 {
                     NamesList.Add(SR1.ReadLine());
@@ -58,7 +69,7 @@ namespace PerzoneFalze
             SurnamesList = new List<string>();
             try
             {
-                StreamReader SR1 = new StreamReader(Directory.GetCurrentDirectory() + "\\TextFiles\\Cognomi.txt");
+                StreamReader SR1 = new StreamReader(Directory.GetCurrentDirectory() + "\\TextFiles\\Cognomi.txt", Encoding.Unicode);
                 while (!SR1.EndOfStream)
                 {
                     SurnamesList.Add(SR1.ReadLine());
@@ -70,6 +81,15 @@ namespace PerzoneFalze
                 return false;
             }
             return true;
+        }
+
+        public DateTime GenerateRandomBirthDate()
+        {
+            Random r = new Random();
+            DateTime BirthDateLowerLimit = new DateTime(1956, 1, 1);
+            DateTime BirthdateUpperLimit = new DateTime(2001, 1, 1);
+            int RandomDateTime_Range = (BirthdateUpperLimit - BirthDateLowerLimit).Days;
+            return BirthDateLowerLimit.AddDays(r.Next(RandomDateTime_Range));
         }
 
     }
