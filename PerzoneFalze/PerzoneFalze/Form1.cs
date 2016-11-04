059bba0f1055d7c81ec20d1888df302533efdd88
David Gray <damnedkaiser@gmail.com>
2016-11-04 20:22:27 +0100

    Aggiunte le variabili per la lista di nomi e cognomi.

--- PerzoneFalze/PerzoneFalze/Form1.cs
+++ PerzoneFalze/PerzoneFalze/Form1.cs
@@ -12,12 +12,24 @@ namespace PerzoneFalze
 {
     public partial class Form1 : Form
     {
+        List<string> NamesList;
+        List<string> SurnamesList;
+
         public Form1()
         {
             InitializeComponent();
             Perzona P1 = new Perzona();
-            MessageBox.Show(P1.Name + " " + P1.Surname + " " + P1.BirthDate.ToString() + " " + P1.DateAdded.ToString() + " " + P1.LastUpdate.ToString() + " " + P1.DeletedDate.ToString() + " " + P1.StateOfMind.ToString());
+            Console.WriteLine(P1.Name + " " + P1.Surname + " " + P1.BirthDate.ToString() + " " + P1.DateAdded.ToString() + " " + P1.LastUpdate.ToString() + " " + P1.DeletedDate.ToString() + " " + P1.StateOfMind.ToString());
         }
 
+        public void  PopulateNamesList()
+        {
+            NamesList = new List<string>();
+        }
+
+        public void PopulateSurnamesList()
+        {
+            SurnamesList = new List<string>();
+        }
     }
 }
