0a2a01580666017e617c66dd526135af6944ff70
ryugiordy <ryugiordy@gmail.com>
2016-11-04 20:13:17 +0100

    Istanziata la form della griglia. Da migliorare.

--- PerzoneFalze/PerzoneFalze/Form1.cs
+++ PerzoneFalze/PerzoneFalze/Form1.cs
@@ -17,6 +17,12 @@ namespace PerzoneFalze
             InitializeComponent();
             Perzona P1 = new Perzona();
             MessageBox.Show(P1.Name + " " + P1.Surname + " " + P1.BirthDate.ToString() + " " + P1.DateAdded.ToString() + " " + P1.LastUpdate.ToString() + " " + P1.DeletedDate.ToString() + " " + P1.StateOfMind.ToString());
+
+            List<Perzona> listaPersone = new List<Perzona>();
+            listaPersone.Add(P1);
+
+            using (frmGrigliaPerzone finestraGriglia = new frmGrigliaPerzone(listaPersone))
+                finestraGriglia.ShowDialog();
         }
 
     }
