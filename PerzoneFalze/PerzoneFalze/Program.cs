6b62346578b67b65135da6f28468452c44aed31e
ryugiordy <ryugiordy@gmail.com>
2016-11-08 22:21:01 +0100

    Aggiunte librerie xpo devexpress

--- PerzoneFalze/PerzoneFalze/Program.cs
+++ PerzoneFalze/PerzoneFalze/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
 
 namespace PerzoneFalze
 {
@@ -14,9 +16,17 @@ namespace PerzoneFalze
         [STAThread]
         static void Main()
         {
+            DevExpress.Skins.SkinManager.EnableFormSkins();
+            DevExpress.LookAndFeel.LookAndFeelHelper.ForceDefaultLookAndFeelChanged();
+            DevExpress.Skins.SkinManager.EnableMdiFormSkins();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(@"XpoProvider=SQLite;Data Source=" + Application.StartupPath + "\\PerzoneFalze.sqlite",AutoCreateOption.SchemaAlreadyExists);
+            XpoDefault.Session = null;
+
         }
     }
 }
